3a94c347982079534680b0d295038d7a0288c818
panwarunionitc <77523007+panwarunionitc@users.noreply.github.com>
2021-05-20 17:59:37 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+define("storeId", ""); //Store ID
+define("daysToExpire", "2");  // Token Expiry Day
+define("live", "no"); // yes if live
+define("hashKey", ""); //hash key provided by Easypay
+define("methods", ""); 
+//MA_PAYMENT_METHOD
+//OTC_PAYMENT_METHOD
+//MA_PAYMENT_METHOD
+//CC_PAYMENT_METHOD
+//empty To use all
+define("UrlBack", "http://localhost:8090/payfund/statusEasypay.php"); // URL where response returned 
+
+
+
+
+?>
